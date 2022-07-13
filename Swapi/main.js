06594eab1a39daabdf511cfb36182f6dc538d420
Sainabs <Sainab.sahal1@gmail.com>
2022-07-13 17:18:56 -0500

    initial commit

---
+++ Swapi/main.js
@@ -0,0 +1,20 @@
+//const { default: axios } = require("axios");
+
+let residentBtn=document.querySelector('button');
+
+function clickedButton(){
+console.log('Button clicked')
+
+axios.get ("https://swapi.dev/api/planets/2/").then(res=>{
+        for(let i=0; i<res.data.residents.length;i++){
+            axios.get(res.data.residents[i]).then(res=>{
+                const h2= document.createElement('h2')
+                h2.textContent=res.data.name
+                document.body.appendChild(h2)
+            })
+        }
+    })
+}
+
+residentBtn.addEventListener('click', clickedButton);
+
